69b83be885d7367f7281c3a7513bb4c94f35e386
Miumaru-iniad <s1f102200637@iniad.org>
2022-09-29 17:15:52 +0900

    create  main window

--- r2fromexcel/polyfit_and_r2.py
+++ r2fromexcel/polyfit_and_r2.py
@@ -1,3 +1,28 @@
-import PySimpleGUI
+import PySimpleGUI as sg
 import numpy
 import openpyxl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+sg.theme("GreenMono")
+frame1 = sg.Frame('',[] , size=(200, 400))
+frame2 = sg.Frame('',[] , size=(400, 400))
+layout = [
+    [
+        frame1,
+        frame2
+    ]
+]
+window = sg.Window('近似関数、相関係数アプリ', layout, resizable=True)
+
+#GUI表示実行部分
+while True:
+    # ウィンドウ表示
+    event, values = window.read()
+
+    #クローズボタンの処理
+    if event is None:
+        print('exit')
+        break
+
+window.close()

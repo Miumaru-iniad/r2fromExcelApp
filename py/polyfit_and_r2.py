43c29c97f84ffd5e85132fe3f5eb826cd82a058a
Miumaru-iniad <s1f102200637@iniad.org>
2022-10-02 14:51:01 +0900

    read_excel

--- py/polyfit_and_r2.py
+++ py/polyfit_and_r2.py
@@ -1,3 +1,5 @@
+from fileinput import filename
+from tabnanny import filename_only
 import PySimpleGUI as sg
 import numpy as np
 import openpyxl
@@ -11,9 +13,12 @@ ax = fig.add_subplot(111)
 def make_data_fig(fig,make = True):
 
     if make:
-        # x = np.linspace(0, 2*np.pi, 500)
-        x = np.arange(0, 2*np.pi, 0.05*np.pi)
-        ax.plot(x, np.sin(x))
+        filename = values["-filename-"]
+        df = pd.read_excel(filename,sheet_name=0)
+        xs = list(df["x"])
+        ys = list(df["y"])
+        print(xs,ys)
+        ax.scatter(xs, ys)
         return fig
 
     else:
@@ -40,43 +45,36 @@ frame1 = sg.Frame('',
             sg.InputText(key='-filename-',enable_events=True,readonly=True),
             sg.FileBrowse('ファイルを選択',target='-filename-',file_types=(('Excell ファイル', '*.xlsx'),))
         ],
-        [
-            sg.Text("次数を選択してください"),sg.Combo([1,2,3,4,5,6], default_value="選択して下さい",readonly=True,size=(30,1))
-        ],
-        [sg.Button('Display',key='-display-'), sg.Button('clear',key='-clear-'), sg.Cancel()],
+        [sg.Button('Display',key='-display-'), sg.Button('clear',key='-clear-')],
         [sg.Canvas(key='-CANVAS-')],
         [
             sg.Text("相関係数"),sg.InputText("r2=",readonly=True,key="-r2-")
         ],
-        [sg.Button("実行",size=(30,2))],
-    ], size=(700, 800)
+    ], size=(700, 650)
 )
 
 frame2 = sg.Frame('',
-    [
+    [    
         [
-            sg.Text('実行結果')
+            sg.Text('チェックを入れると近似式とグラフを表示する')
         ],
         [
-            sg.Text("近似式")
+            sg.Checkbox("D=1", enable_events=True, key="-d1-"),sg.InputText("ここに数式が出ます",readonly=True,key="-f1-")
         ],
         [
-            sg.Text("1"),sg.InputText("ここに数式が出ます",readonly=True,key="-f1-")
+            sg.Checkbox("D-2", enable_events=True, key="-d2-"),sg.InputText("ここに数式が出ます",readonly=True,key="-f2-")
         ],
         [
-            sg.Text("2"),sg.InputText("ここに数式が出ます",readonly=True,key="-f2-")
+            sg.Checkbox("D-3", enable_events=True, key="-d3-"),sg.InputText("ここに数式が出ます",readonly=True,key="-f3-")
         ],
         [
-            sg.Text("3"),sg.InputText("ここに数式が出ます",readonly=True,key="-f3-")
+            sg.Checkbox("D-4", enable_events=True, key="-d4-"),sg.InputText("ここに数式が出ます",readonly=True,key="-f4-")
         ],
         [
-            sg.Text("4"),sg.InputText("ここに数式が出ます",readonly=True,key="-f4-")
+            sg.Checkbox("D-5", enable_events=True, key="-d5-"),sg.InputText("ここに数式が出ます",readonly=True,key="-f5-")
         ],
         [
-            sg.Text("5"),sg.InputText("ここに数式が出ます",readonly=True,key="-f5-")
-        ],
-        [
-            sg.Text("6"),sg.InputText("ここに数式が出ます",readonly=True,key="-f6-")
+            sg.Checkbox("D-6", enable_events=True, key="-d6-"),sg.InputText("ここに数式が出ます",readonly=True,key="-f6-")
         ],
         [
             sg.Text("結果をエクセルに記入する")
@@ -84,7 +82,7 @@ frame2 = sg.Frame('',
         [
             sg.Button("記入",size=(25,1)),sg.Button("終了",size=(25,1))
         ],
-    ] , size=(600, 800)
+    ] , size=(520, 650)
 )
 
 layout = [
@@ -107,6 +105,11 @@ while True:
     if event in (None,"Cancel","終了"):
         print('exit')
         break
+
+    elif event == '-filename-':
+        fig = make_data_fig(fig, make=True)
+        fig_agg.draw()
+
 
     elif event == '-display-':
         fig = make_data_fig(fig, make=True)
